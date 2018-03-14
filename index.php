c0f747c3612f304568c300bdc1816c0a1be16384
ArthurGMart <arthurgonzie@gmail.com>
2018-03-14 09:14:55 +0100

    A few minor changes

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>NIEUWSBRIEF WEBAPP</title>
 </head>
 <body>
-<?php
-echo 'Hello world';
-?>
+<h1>NIEUWSBRIEF</h1>
+<H2>Voor bezoekers</H2>
+<a href="inschrijven.php">Inschrijven</a><br>
+<a href="uitschrijven.php">Uitschrijven</a><br>
+<h2>Voor admins</h2>
+<a href="versturen.php">Nieuwsbrief versturen</a><br>
+<a href="beheren.php">Mailingijst beheren</a><br>
 </body>
 </html>
