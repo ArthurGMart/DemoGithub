89f10f1e0911ccc623b1a723560d4c35ab046a1a
ArthurGMart <arthurgonzie@gmail.com>
2018-03-14 09:37:36 +0100

    A few minor changes

--- verwerk_verzending.php
+++ verwerk_verzending.php
@@ -1,6 +1,9 @@
 <?php
+
+// 0. Uitlezen van de POST-array
+
 // 1. Connectie maken met de DB
 
 // 2. Kijken in de database en alle mailadressen tevoorschijn brengen
 
-// 3. Loopje waarin een bericht
+// 3. Loopje waarin een bericht wordt verzonden naar alle mailadressen
