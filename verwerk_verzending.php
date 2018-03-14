bdad20b305a43f4b2d2ca4a1ec6aa15395e70493
ArthurGMart <arthurgonzie@gmail.com>
2018-03-14 11:09:27 +0100

    A few changes

--- verwerk_verzending.php
+++ verwerk_verzending.php
@@ -1,9 +1,14 @@
 <?php
 
 // 0. Uitlezen van de POST-array
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
 // 1. Connectie maken met de DB
+$dbc = mysqli_connect('localhost', 'root', 'root', 'arthur_db') or die ('error connecting');
 
 // 2. Kijken in de database en alle mailadressen tevoorschijn brengen
+$query = "SELECT mailadres FROM nieuwsbrief_tutorial";
+$result = mysqli_query($dbc, $query) or die ('Error querying');
 
 // 3. Loopje waarin een bericht wordt verzonden naar alle mailadressen
