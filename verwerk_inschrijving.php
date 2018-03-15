4b866d2abbcf796996b3c001ad9263c84d8e0c3b
ArthurGMart <arthurgonzie@gmail.com>
2018-03-15 10:55:46 +0100

    A few changes

---
+++ verwerk_inschrijving.php
@@ -0,0 +1,10 @@
+<?php
+    $voornaam = $_POST['voornaam'];
+    $tussenvoegsel = $_POST['tussenvoegsel'];
+    $achternaam = $_POST['achternaam'];
+    $mailadres = $_POST['mailadres'];
+
+    echo $voornaam . '<br>' ;
+echo $tussenvoegsel . '<br>' ;
+echo $achternaam . '<br>' ;
+echo $mailadres . '<br>' ;
