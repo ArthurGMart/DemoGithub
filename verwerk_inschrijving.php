0d5047843cdc0a49c2ba8845f3b870704e645137
ArthurGMart <arthurgonzie@gmail.com>
2018-03-15 11:26:05 +0100

    A few changes

--- verwerk_inschrijving.php
+++ verwerk_inschrijving.php
@@ -1,10 +1,23 @@
 <?php
-    $voornaam = $_POST['voornaam'];
-    $tussenvoegsel = $_POST['tussenvoegsel'];
-    $achternaam = $_POST['achternaam'];
-    $mailadres = $_POST['mailadres'];
 
-    echo $voornaam . '<br>' ;
+// POST-ARRAY UITLEZEN
+$voornaam = $_POST['voornaam'];
+$tussenvoegsel = $_POST['tussenvoegsel'];
+$achternaam = $_POST['achternaam'];
+$mailadres = $_POST['mailadres'];
+
+// DATA IN DATABASE STOPPEN
+// 1. VERBINDING MAKEN MET DATABASE
+mysqli_connect('localhost','root','','arthur_db') or die ('Error connecting.');
+// 2. OPDRACHT (QUERY) SCHRIJVEN VOOR DE DATABASE
+
+// 3. QUERY UITVOEREN
+
+// 4. VERBINDING VERBREKEN
+
+//  BEVESTIGEN DAT DATA IN DE DATABASE STAAT
+echo 'De volgende gegevens zijn toegevoegd aan de database:<br>';
+echo $voornaam . '<br>' ;
 echo $tussenvoegsel . '<br>' ;
 echo $achternaam . '<br>' ;
 echo $mailadres . '<br>' ;
