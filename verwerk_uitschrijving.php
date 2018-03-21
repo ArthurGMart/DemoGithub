39777019f29c81eaed628905b84270eaec4f38cc
ArthurGMart <arthurgonzie@gmail.com>
2018-03-21 11:29:37 +0100

    ``

--- verwerk_uitschrijving.php
+++ verwerk_uitschrijving.php
@@ -1,8 +1,14 @@
 <?php
 // FORMULIER UITLEZEN (DATA BINNENHALEN)
-
+$mailadres = $_POST['mailadres'];
 // CONNECTIE MAKEN MET DE DATABASE
+$host     = 'localhost';
+$username = 'arthur';
+$password = '12435';
+$dbname   = 'arthur_db';
 
+$dbc = mysqli_connect($host,$username,$password,$dbname) or die ('Error connecting');
+echo 'Connectie geslaagd';
 // QUERY BEDENKEN
 
 // QUERY UITVOEREN
