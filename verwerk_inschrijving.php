1f85aa91eb1ead607fe63e01a7247492f608af40
ArthurGMart <arthurgonzie@gmail.com>
2018-03-21 08:47:23 +0100

    A few changes

--- verwerk_inschrijving.php
+++ verwerk_inschrijving.php
@@ -10,10 +10,11 @@ $mailadres = $_POST['mailadres'];
 // 1. VERBINDING MAKEN MET DATABASE
 $dbc = mysqli_connect('localhost','root','','arthur_db') or die ('Error connecting.');
 // 2. OPDRACHT (QUERY) SCHRIJVEN VOOR DE DATABASE
-
+$query = "INSERT INTO nieuwsbrief_tutorial VALUES ('$voornaam','$tussenvoegsel','$achternaam','$mailadres')";
 // 3. QUERY UITVOEREN
-
+$result = mysqli_query($dbc,$query) or die ('Error querying.');
 // 4. VERBINDING VERBREKEN
+mysqli_close($dbc);
 
 //  BEVESTIGEN DAT DATA IN DE DATABASE STAAT
 echo 'De volgende gegevens zijn toegevoegd aan de database:<br>';
